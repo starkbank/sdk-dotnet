cfff380757e31f8990644fa9e04c2e4baacaae95
Caio Dottori <caio.dottori@starkbank.com>
2020-04-08 18:05:41 -0300

    added Utils.Resource

---
+++ StarkBank/StarkBank/Utils/Resource.cs
@@ -0,0 +1,11 @@
+﻿namespace StarkBank.Utils
+{
+    public class Resource
+    {
+        public string ID { get; }
+        public Resource(string id)
+        {
+            ID = id;
+        }
+    }
+}
