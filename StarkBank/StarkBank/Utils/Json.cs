6a20b4a853c99fdfcce22793dfad1fa66db6c490
Caio Dottori <caio.dottori@starkbank.com>
2020-04-08 20:08:56 -0300

    added Utils.Json

---
+++ StarkBank/StarkBank/Utils/Json.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace StarkBank.Utils
+{
+    internal static class Json
+    {
+        internal static string Encode(object payload)
+        {
+            return JsonConvert.SerializeObject(payload);
+        }
+
+        internal static JObject Decode(string content)
+        {
+            return JObject.Parse(content) as JObject;
+        }
+    }
+}
