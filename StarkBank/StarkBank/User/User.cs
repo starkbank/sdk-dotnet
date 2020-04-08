52797b805e3ba6d3a34c64b09ea3d35354ffc921
Caio Dottori <caio.dottori@starkbank.com>
2020-04-08 18:05:56 -0300

    added User and Project

---
+++ StarkBank/StarkBank/User/User.cs
@@ -0,0 +1,28 @@
+﻿using StarkBank.Utils;
+
+namespace StarkBank
+{
+    public abstract class User : Resource
+    {
+        public string Pem { get; }
+        public string Environment { get; }
+        readonly private string Kind;
+
+        public User(string id, string privateKey, string environment, string kind) : base(id)
+        {
+            Pem = Checks.CheckPrivateKey(privateKey);
+            Environment = Checks.CheckEnvironment(environment);
+            Kind = kind;
+        }
+
+        internal string AccessId()
+        {
+            return Kind + "/" + ID;
+        }
+
+        internal EllipticCurve.PrivateKey PrivateKey()
+        {
+            return EllipticCurve.PrivateKey.fromPem(Pem);
+        }
+    }
+}
