e8bd5dc65d53fb58227603968bf7544adc7f81c6
Caio Dottori <caio.dottori@starkbank.com>
2020-04-08 21:15:09 -0300

    added Utils.Url

---
+++ StarkBank/StarkBank/Utils/Url.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+namespace StarkBank.Utils
+{
+    internal static class Url
+    {
+        internal static string Encode(Dictionary<string, object> query)
+        {
+            List<string> queryStringList = new List<string>();
+
+            foreach (KeyValuePair<string, object> entry in query)
+            {
+                if (entry.Value == null)
+                {
+                    continue;
+                }
+
+                string key = Case.UpperCamelToLowerCamel(entry.Key);
+
+                string value = "";
+                if (IsList(entry.Value))
+                {
+                    value = string.Join(",", entry.Value);
+                } else
+                {
+                    value = entry.Value.ToString();
+                }
+
+                queryStringList.Add(key + "=" + value);
+            }
+
+            if (queryStringList.Count > 0)
+            {
+                return "?" + string.Join("&", queryStringList);
+            }
+
+            return "";
+        }
+
+        private static bool IsList(object o)
+        {
+            if (o == null) return false;
+            return o is IList &&
+                   o.GetType().IsGenericType &&
+                   o.GetType().GetGenericTypeDefinition().IsAssignableFrom(typeof(List<>));
+        }
+    }
+}
