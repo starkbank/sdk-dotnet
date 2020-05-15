acaa72e21858c3a9d2d61ac26113e9723bad7d95
Caio Dottori <caio.dottori@starkbank.com>
2020-05-15 19:57:41 -0300

    Refactor test-user to use environment variables instead of hardcoded credentials

--- StarkBank/StarkBankTests/User.cs
+++ StarkBank/StarkBankTests/User.cs
@@ -1,4 +1,5 @@
-﻿using StarkBank;
+﻿using System;
+using StarkBank;
 
 
 namespace StarkBankTests
@@ -9,8 +10,8 @@ namespace StarkBankTests
         {
             Project project = new Project(
                 "sandbox",
-                "9999999999999999",
-                "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEIBEcEJZLk/DyuXVsEjz0w4vrE7plPXhQxODvcG1Jc0WToAcGBSuBBAAK\noUQDQgAE6t4OGx1XYktOzH/7HV6FBukxq0Xs2As6oeN6re1Ttso2fwrh5BJXDq75\nmSYHeclthCRgU8zl6H1lFQ4BKZ5RCQ==\n-----END EC PRIVATE KEY-----\n"
+                Environment.GetEnvironmentVariable("SANDBOX_ID"), // "9999999999999999",
+                Environment.GetEnvironmentVariable("SANDBOX_PRIVATE_KEY") // "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEIBEcEJZLk/DyuXVsEjz0w4vrE7plPXhQxODvcG1Jc0WToAcGBSuBBAAK\noUQDQgAE6t4OGx1XYktOzH/7HV6FBukxq0Xs2As6oeN6re1Ttso2fwrh5BJXDq75\nmSYHeclthCRgU8zl6H1lFQ4BKZ5RCQ==\n-----END EC PRIVATE KEY-----\n"
             );
             User.Default = project;
             return project;
