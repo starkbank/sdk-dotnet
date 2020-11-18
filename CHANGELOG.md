c6c06d5214a58d259d8ebef2facf154cf3acc50d
Caio Dottori <caio.dottori@starkbank.com>
2020-11-18 19:20:20 -0300

    Update version to 2.2.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,8 @@ Given a version number MAJOR.MINOR.PATCH, increment:
 
 
 ## [Unreleased]
+
+## [2.2.3] - 2020-11-18
 ### Fixes
 - Fixed bad midnight DateTime string conversions
 
--- StarkBank/StarkBank/Utils/Request.cs
+++ StarkBank/StarkBank/Utils/Request.cs
@@ -38,7 +38,7 @@ namespace StarkBank.Utils
         private static HttpClient makeClient()
         {
             HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("User-Agent", $".NET-{Environment.Version}-SDK-2.2.2");
+            client.DefaultRequestHeaders.Add("User-Agent", $".NET-{Environment.Version}-SDK-2.2.3");
             return client;
         }
 
