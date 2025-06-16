ad3ce3a2c9d5a7edaaaed8f12f23d3e140a560bd
Paulo Souza <paulo.souza@starkbank.com>
2025-06-16 20:10:25 -0300

    Wip

--- StarkBank/StarkBankTests/InvoicePullRequest.cs
+++ StarkBank/StarkBankTests/InvoicePullRequest.cs
@@ -12,52 +12,40 @@ namespace StarkBankTests
         public readonly User user = TestUser.SetDefaultProject();
 
         [Fact]
-        public void Create()
+        public void CreateAndRetry()
         {
             List<InvoicePullSubscription> subscriptions = InvoicePullSubscription.Query(limit: 1, status: "active").ToList();
             InvoicePullSubscription subscription = InvoicePullSubscription.Get(subscriptions.First().ID, user);
             
             string invoiceId = subscriptions.First().Data["invoiceId"]?.ToString();
             string subscriptionId = subscriptions.First().ID;
+
             List<InvoicePullRequest> requests = InvoicePullRequest.Create(new List<InvoicePullRequest> { Example("default", invoiceId, subscriptionId) }, user);
+            List<InvoicePullRequest> requests = InvoicePullRequest.Create(new List<InvoicePullRequest> { Example("retry", invoiceId, subscriptionId) }, user);
             InvoicePullRequest request = requests.First();
+
             TestUtils.Log(request);
             Assert.NotNull(request.ID);
         }
 
-        // [Fact]
-        // public void Retry()
-        // {
-        //     List<InvoicePullRequest> requests = InvoicePullRequest.Create(new List<InvoicePullRequest> { Example("qrcode") }, user);
-        //     InvoicePullRequest request = requests.First();
-        //     TestUtils.Log(request);
-        //     Assert.NotNull(request.ID);
-        //     Assert.NotNull(request.Name);
-        //     Assert.NotNull(request.TaxID);
-        // }
+        [Fact]
+        public void Query()
+        {
+            List<InvoicePullRequest> requests = InvoicePullRequest.Query(limit: 5).ToList();
+            foreach (InvoicePullRequest request in requests)
+            {
+                TestUtils.Log(request);
+                Assert.NotNull(request.ID);
+            }
+        }
 
-        // [Fact]
-        // public void Query()
-        // {
-        //     List<InvoicePullRequest> requests = InvoicePullRequest.Query(limit: 5).ToList();
-        //     foreach (InvoicePullRequest request in requests)
-        //     {
-        //         TestUtils.Log(request);
-        //         Assert.NotNull(request.ID);
-        //         Assert.NotNull(request.Name);
-        //         Assert.NotNull(request.TaxID);
-        //     }
-        // }
-
-        // [Fact]
-        // public void QueryAndGet()
-        // {
-        //     List<InvoicePullRequest> requests = InvoicePullRequest.Query(limit: 1).ToList();
-        //     InvoicePullRequest invoice = InvoicePullRequest.Get(requests.First().ID, user);
-        //     Assert.Equal(invoice.ID, requests.First().ID);
-        //     Assert.Equal(invoice.Name, requests.First().Name);
-        //     Assert.Equal(invoice.TaxID, requests.First().TaxID);
-        // }
+        [Fact]
+        public void QueryAndGet()
+        {
+            List<InvoicePullRequest> requests = InvoicePullRequest.Query(limit: 1).ToList();
+            InvoicePullRequest request = InvoicePullRequest.Get(requests.First().ID, user);
+            Assert.Equal(request.ID, requests.First().ID);
+        }
 
         internal static InvoicePullRequest Example(string attemptType, string invoiceId = null, string subscriptionId = null)
         {
